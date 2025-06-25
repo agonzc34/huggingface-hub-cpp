3f8b1ab661d5b7acf83f285bfdbd252709438639
agonzc34 <agonzc34@users.noreply.github.com>
2025-06-25 14:42:36 +0000

    deploy: b35a584ce224c3a539814c457526d1a764c503ea

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['get_5fmodel_5fmetadata_5ffrom_5fhf_0',['get_model_metadata_from_hf',['../huggingface__hub_8h.html#aa34e943029e0aff3b708b6801c108953',1,'huggingface_hub']]]
+  ['get_5fmodel_5fmetadata_5ffrom_5fhf_0',['get_model_metadata_from_hf',['../huggingface__hub_8h.html#a074111684a60359c5b9539a74b410125',1,'huggingface_hub']]]
 ];
